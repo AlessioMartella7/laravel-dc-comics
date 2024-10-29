280c0fc5a42df80d5a5a30e2bae34feaad47eb9b
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-29 15:16:24 +0100

    first setup

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/', [PageController::class, 'home'])->name('home');

96d6bc55e9ca53dcc4f12880fe0d07a255283496
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-29 15:34:04 +0100

    added Sword model and  msc

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\SwordController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/swords', [SwordController::class, 'index'])->name('swords.index');
+

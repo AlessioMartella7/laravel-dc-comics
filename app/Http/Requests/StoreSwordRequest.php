35e6be7daaed84ee9a5dbab7c6efd1613e08a780
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-31 16:41:43 +0100

    added custom function messages in StoreSwordRequest

--- app/Http/Requests/StoreSwordRequest.php
+++ app/Http/Requests/StoreSwordRequest.php
@@ -30,4 +30,22 @@ class StoreSwordRequest extends FormRequest
             'type' => 'required|string|min:4',
         ];
     }
+    public function messages(){
+        return[
+            'name.required' => 'Il nome è un parametro obbligatorio',
+            'name.alpha' => 'Il nome deve contenere solo caratteri',
+            'name.min' => 'Il nome deve contenere minimo due caratteri',
+            'weight.required' => 'Il peso è un parametro obbligatorio',
+            'weight.numeric' => 'Il peso deve essere un numero',
+            'image.required' => 'L\'immagine è un parametro obbligatorio',
+            'image.url' => 'Inserire un URL per l\'immagine',
+            'length.required' => 'La lunghezza è un parametro obbligatorio',
+            'lenght.numeric' => 'La lunghezza deve essere un numero',
+            'material.required' => 'Il materiale è un parametro obbligatorio',
+            'material.min' => 'Il materiale deve contenere minimo tre caratteri',
+            'type.required' => 'Il tipo è un parametro obbligatorio',
+            'type.min' => 'Il tipo deve contenere minimo tre caratteri',
+
+        ];
+    }
 }
